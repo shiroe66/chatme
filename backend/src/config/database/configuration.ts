20ca6dc6e1b372fc1b2dd536e57b1fa72e775009
shiroe66 <shiroeqky@gmail.com>
2023-02-15 00:05:06 +0500

    fix: environments

--- backend/src/config/database/configuration.ts
+++ backend/src/config/database/configuration.ts
@@ -1,9 +1,9 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('psql', () => ({
-  host: process.env.POSTGRESQL_HOST,
-  port: process.env.POSTGRESQL_PORT,
-  username: process.env.POSTGRESQL_USERNAME,
-  password: process.env.POSTGRESQL_PASSWORD,
-  database: process.env.POSTGRESQL_DATABASE,
+  host: process.env.PSQL_HOST,
+  port: process.env.PSQL_PORT,
+  username: process.env.PSQL_USERNAME,
+  password: process.env.PSQL_PASSWORD,
+  database: process.env.PSQL_DATABASE,
 }));
